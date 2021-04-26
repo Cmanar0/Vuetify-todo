6ede2f83a3a93c7e68e8636871ea69a2d6e5a8bb
Cmanar0 <56640205+Cmanar0@users.noreply.github.com>
2021-04-26 02:04:40 +0200

    Initial commit

---
+++ src/components/Tools/API.vue
@@ -0,0 +1,40 @@
+<template>
+<div id="app">
+  <ul>
+    <v-list-item 
+      three-line
+      v-for="post in info" 
+      :key="post.symbol"
+      >
+      <v-list-item-content>
+        <v-list-item-title>{{post.code}}</v-list-item-title>
+        <v-list-item-subtitle>
+          <p class="mb-0">Rate: {{post.rate}}</p>
+        </v-list-item-subtitle>
+        <v-list-item-subtitle>
+          {{post.description}}
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </ul>
+
+</div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      info: null,
+    }
+  },
+  mounted () {
+    axios
+    .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(response => (this.info = response.data.bpi))
+    .then(response => (console.log(this.info)))
+
+  }
+}
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about pa-6">
+    <h1>Coindesk API</h1>
+    <p>Current price of Bitcoin in different currencies displayed from coindesk API:</p>
+    <api />
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data: () => ({
+      drawer: null,
+      items: [
+        { title: 'Todo', icon: 'mdi-format-list-checks', to: '/' },
+        { title: 'About', icon: 'mdi-help-box', to: '/about' },
+      ],
+    }),
+    components: {
+      'api': require('@/components/Tools/API.vue').default
+    }
+  }
+</script>
